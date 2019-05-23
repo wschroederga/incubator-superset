c587ef1a399d60ff5e696353a89874cda5373c46
Schroeder, Walter Demian <wschroeder@grupoassa.com>
2019-05-23 12:29:29 -0300

    old Key

--- superset/config.py
+++ superset/config.py
@@ -73,7 +73,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # ---------------------------------------------------------
 
 # Your App secret key
-SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
+SECRET_KEY = 'mqYZVcxashBCUVRcnnii'  # noqa
 
 # The SQLAlchemy connection string.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DATA_DIR, 'superset.db')
